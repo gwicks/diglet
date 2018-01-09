a31f57389553cb506665db9708eddcd76ba69315
Greg Wicks <gwicks95@gmail.com>
2018-01-09 11:23:52 -0500

    Refactor command structure a bit

--- commands/compile.go
+++ commands/compile.go
@@ -12,37 +12,48 @@ import (
 
 var rootJSON interface{}
 
+func doCompile(filePath string, targetPath string) error {
+	fd, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+
+	json.Unmarshal(fd, &rootJSON)
+
+	resultJSON, _ := utils.ParseFileRefs(filePath, rootJSON)
+
+	if resultJSONObj, ok := resultJSON.(map[string]interface{}); ok {
+		finalJSON, _ := utils.ParseFileParent(filePath, resultJSONObj)
+
+		validatedJSON, verr := utils.ParseFileSchema(filePath, finalJSON)
+		if verr != nil {
+			log.Error(verr)
+		} else {
+			resultStr, _ := json.MarshalIndent(validatedJSON, "", "    ")
+
+			if len(targetPath) > 0 {
+				ioutil.WriteFile(targetPath, resultStr, 0644)
+			} else {
+				fmt.Println(string(resultStr))
+			}
+		}
+	} else {
+		resultStr, _ := json.MarshalIndent(resultJSON, "", "    ")
+
+		fmt.Println(string(resultStr))
+	}
+	return nil
+}
+
 func compileAction(c *cli.Context) error {
 	cmdArgs := c.Args()
 	if len(cmdArgs) > 0 {
-
-		fd, err := ioutil.ReadFile(cmdArgs[0])
-		if err != nil {
-			log.Error(err)
-			return err
-		}
-
-		json.Unmarshal(fd, &rootJSON)
-
-		resultJSON, _ := utils.ParseFileRefs(cmdArgs[0], rootJSON)
-
-		if resultJSONObj, ok := resultJSON.(map[string]interface{}); ok {
-			finalJSON, _ := utils.ParseFileParent(cmdArgs[0], resultJSONObj)
-
-			validatedJSON, verr := utils.ParseFileSchema(cmdArgs[0], finalJSON)
-			if verr != nil {
-				log.Error(verr)
-			} else {
-				resultStr, _ := json.MarshalIndent(validatedJSON, "", "    ")
-
-				fmt.Println(string(resultStr))
-			}
+		if len(cmdArgs) == 2 {
+			doCompile(cmdArgs[0], cmdArgs[1])
 		} else {
-			resultStr, _ := json.MarshalIndent(resultJSON, "", "    ")
-
-			fmt.Println(string(resultStr))
+			doCompile(cmdArgs[0], "")
 		}
-
 	} else {
 		fmt.Println("Must specify a JSON file to compile")
 	}
