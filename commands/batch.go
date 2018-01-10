315ec5f30a2e8b342b5e666cf344bd55e9a34284
Greg Wicks <gwicks95@gmail.com>
2018-01-10 13:39:20 -0500

    Implement a worker bool for batch processing

--- commands/batch.go
+++ commands/batch.go
@@ -2,10 +2,13 @@ package commands
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"os"
 	"strings"
+	"sync"
 
+	"github.com/dc0d/workerpool"
 	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 )
@@ -23,22 +26,33 @@ func batchAction(c *cli.Context) error {
 		scanner := bufio.NewScanner(inFile)
 		scanner.Split(bufio.ScanLines)
 
+		jobs := make(chan func(), 10)
+
+		myAppCtx, myAppCancel := context.WithCancel(context.Background())
+		_ = myAppCancel
+
+		pool, _ := workerpool.WithContext(myAppCtx, -1, jobs)
+
+		mutex := &sync.Mutex{}
 		for scanner.Scan() {
 			batchline := scanner.Text()
 
 			taskData := strings.Split(batchline, " ")
 
 			if len(taskData) == 2 {
-				fmt.Print("Processing ")
-				fmt.Print(taskData[0])
-				fmt.Print(" into ")
-				fmt.Println(taskData[1])
-				cerr := doCompile(taskData[0], taskData[1])
-				if cerr != nil {
-					return cerr
+				fmt.Println(fmt.Sprintf("Processing %s into %s", taskData[0], taskData[1]))
+				jobs <- func() {
+					mutex.Lock()
+					cerr := doCompile(taskData[0], taskData[1])
+					mutex.Unlock()
+					if cerr != nil {
+						log.Error(cerr)
+					}
 				}
+
 			}
 		}
+		pool.StopWait()
 	} else {
 		fmt.Println("Must specify a batch file to compile")
 	}
